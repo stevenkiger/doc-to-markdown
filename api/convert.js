ed1d33c2201909e7d1fd3f0bcf0fcb0415bd77f1
stevenkiger <steven.kiger@incubeta.com>
2026-04-07 17:24:07 -0600

    Add files via upload

--- api/convert.js
+++ api/convert.js
@@ -1,3 +1,5 @@
+export const config = { api: { bodyParser: { sizeLimit: '20mb' } } };
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
